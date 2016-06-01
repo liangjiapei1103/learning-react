64740061d7a36cc8a8590e7915740bf33f56986d
Jiapei Liang <liangjiapei@Jiapeis-MacBook-Pro.local>
2016-06-01 13:55:07 -0400

    Create Contacts-app components

--- contact-app/app/App.js
+++ contact-app/app/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {render} from 'react-dom';
 
 // Main component. Renders a SearchBar and a ContactList
@@ -15,3 +15,47 @@ class ContactsApp extends Component {
 ContactsApp.propTypes = {
     contacts: PropTypes.arrayOf(PropTypes.object)
 }
+
+class SearchBar extends Component {
+    render() {
+        return <input type="search" placeholder="search" />
+    }
+}
+
+class ContactList extends Component {
+    render() {
+        return (
+            <ul>
+                {this.props.contacts.map(
+                    (contact) => <ContactItem key={contact.email}
+                                              name={contact.name}
+                                              email={contact.email} />
+                )}
+            </ul>
+        );
+    }
+}
+ContactList.propTypes = {
+    contacts: PropTypes.arrayOf(PropTypes.object)
+}
+
+class ContactItem extends Component {
+    render() {
+        return <li>{this.props.name} - {this.props.email}</li>
+    }
+}
+ContactItem.propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired
+}
+
+let contacts = [
+    { name: "Cassio Zen", email: "cassiozen@gmail.com" },
+    { name: "Dan Abramov", email: "gaearon@somewhere.com" },
+    { name: "Pete Hunt", email: "floydophone@somewhere.com" },
+    { name: "Paul O’Shannessy", email: "zpao@somewhere.com" },
+    { name: "Ryan Florence", email: "rpflorence@somewhere.com" },
+    { name: "Sebastian Markbage", email: "sebmarkbage@here.com" },
+];
+
+render(<ContactsApp contacts={contacts} />, document.getElementById('root'));
